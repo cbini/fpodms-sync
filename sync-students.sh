5de74d1e564220e39a59516a5ebee51735cf26a7
Charlie Bini <cbini87@gmail.com>
2021-08-16 20:26:23 +0000

    sync script

---
+++ sync-students.sh
@@ -0,0 +1,2 @@
+~/quotaguard/bin/qgtunnel python3.6 ~/datarobot/db_extracts/db_extracts.py --config ~/datarobot/db_extracts/config/fpodms.json
+~/.virtualenvs/fpodms/bin/python ~/datarobot/fpodms/sync-students.py
