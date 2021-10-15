f4a41049b58bd54f2ab8e07ac3ce94161764b0b0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-10-15 21:18:56 +0000

    update path

--- fpodms_sync/sync-students.sh
+++ fpodms_sync/sync-students.sh
@@ -2,4 +2,4 @@ cd ~/datarobot/datagun
 ./bin/qgtunnel ~/.pyenv/versions/datagun/bin/python ./datagun/extract.py -C ./datagun/config/fpodms.json
 
 cd ~/datarobot/fpodms
-~/.virtualenvs/fpodms/bin/python sync-students.py
+~/.pyenv/versions/fpodms-sync/bin/python ./fpodms_sync/sync-students.py
