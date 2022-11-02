cfcd5e2af8e4960c1dabdfcc1d359834d73b0c39
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-02 09:45:17 -0500

    cleanuop

--- fpodms_sync/extract.py
+++ fpodms_sync/extract.py
@@ -6,54 +6,54 @@ import pathlib
 import fpodms
 from google.cloud import storage
 
-FPODMS_USERNAME = os.getenv("FPODMS_USERNAME")
-FPODMS_PASSWORD = os.getenv("FPODMS_PASSWORD")
-GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
-CURRENT_SCHOOL_YEAR_ID = int(os.getenv("CURRENT_ACADEMIC_YEAR"))
-PROJECT_PATH = pathlib.Path(__file__).absolute().parent
-
 
 def main():
-    gcs_client = storage.Client()
-    gcs_bucket = gcs_client.bucket(GCS_BUCKET_NAME)
+    file_dir = pathlib.Path(__file__).absolute().parent
+    data_dir = file_dir / "data"
 
-    fp = fpodms.FPODMS(email_address=FPODMS_USERNAME, password=FPODMS_PASSWORD)
+    current_school_year_id = int(os.getenv("CURRENT_ACADEMIC_YEAR"))
+
+    gcs_client = storage.Client()
+    gcs_bucket = gcs_client.bucket(os.getenv("GCS_BUCKET_NAME"))
+
+    fp = fpodms.FPODMS(
+        email_address=os.getenv("FPODMS_USERNAME"),
+        password=os.getenv("FPODMS_PASSWORD"),
+    )
 
     all_years = fp.api.all_years()
     current_school_year_name = next(
-        iter([y for y in all_years if y["id"] == CURRENT_SCHOOL_YEAR_ID]), None
+        iter([y for y in all_years if y["id"] == current_school_year_id]), None
     ).get("name")
-
-    data_path = PROJECT_PATH / "data"
 
     # get all assessment exports
     for fn in fp.export.all_exports:
         print(fn.__name__)
-        export = fn(year=CURRENT_SCHOOL_YEAR_ID)
+        export = fn(year=current_school_year_id)
         export_data = export.data
 
-        data_path = PROJECT_PATH / "data" / fn.__name__
-        if not data_path.exists():
-            data_path.mkdir(parents=True)
-            print(f"\tCreated {'/'.join(data_path.parts[-3:])}...")
+        export_dir = file_dir / "data" / fn.__name__
+        if not export_dir.exists():
+            export_dir.mkdir(parents=True)
+            print(f"\tCreated {'/'.join(export_dir.parts[-3:])}...")
 
-        data_filename = data_path / export.filename
-        with data_filename.open("w") as f:
+        export_filepath = export_dir / export.filename
+        with export_filepath.open("w") as f:
             f.write(export_data)
-        print(f"\tSaved '{export.filename}' to {data_filename}")
+        print(f"\tSaved '{export.filename}' to {export_filepath}")
 
-        destination_blob_name = "fpodms/" + "/".join(data_filename.parts[-2:])
+        destination_blob_name = "fpodms/" + "/".join(export_filepath.parts[-2:])
         blob = gcs_bucket.blob(destination_blob_name)
-        blob.upload_from_filename(data_filename)
+        blob.upload_from_filename(export_filepath)
         print(f"\tUploaded to {destination_blob_name}!\n")
 
     # get all classroom data
     endpoint_name = "bas_classes"
-    schools = fp.api.school_by_district(school_year_id=CURRENT_SCHOOL_YEAR_ID)
+    schools = fp.api.school_by_district(school_year_id=current_school_year_id)
     for s in schools:
         print(f"{s.get('name')} classes...")
         school_id = s.get("id")
-        classes = fp.api.basclass_by_school(school_id, CURRENT_SCHOOL_YEAR_ID)
+        classes = fp.api.basclass_by_school(school_id, current_school_year_id)
 
         classes_updated = []
         for c in classes:
@@ -62,22 +62,22 @@ def main():
             )
             classes_updated.append(c)
 
-        data_path = PROJECT_PATH / "data" / endpoint_name
-        if not data_path.exists():
-            data_path.mkdir(parents=True)
-            print(f"\tCreated {'/'.join(data_path.parts[-3:])}...")
+        endpoint_dir = data_dir / endpoint_name
+        if not endpoint_dir.exists():
+            endpoint_dir.mkdir(parents=True)
+            print(f"\tCreated {'/'.join(endpoint_dir.parts[-3:])}...")
 
         classes_filename = (
             f"{endpoint_name}_{school_id}_{s.get('schoolYearId')}.json.gz"
         )
-        data_filename = data_path / classes_filename
-        with gzip.open(data_filename, "wt", encoding="utf-8") as f:
+        classes_filepath = endpoint_dir / classes_filename
+        with gzip.open(classes_filepath, "wt", encoding="utf-8") as f:
             json.dump(classes_updated, f)
-        print(f"\tSaved '{classes_filename}' to {data_filename}")
+        print(f"\tSaved '{classes_filename}' to {classes_filepath}")
 
-        destination_blob_name = "fpodms/" + "/".join(data_filename.parts[-2:])
+        destination_blob_name = "fpodms/" + "/".join(classes_filepath.parts[-2:])
         blob = gcs_bucket.blob(destination_blob_name)
-        blob.upload_from_filename(data_filename)
+        blob.upload_from_filename(classes_filepath)
         print(f"\tUploaded to {destination_blob_name}!\n")
 
 
